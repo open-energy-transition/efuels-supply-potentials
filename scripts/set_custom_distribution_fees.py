d56e0eed935bc02bfe1b394e126ccf2aa4278d4c
Emmanuel Bolarinwa <gbotemibolarinwa@gmail.com>
2025-06-16 16:47:50 +0100

    changed function name to attach buses

--- scripts/set_custom_distribution_fees.py
+++ scripts/set_custom_distribution_fees.py
@@ -16,9 +16,9 @@ from scripts._helper import mock_snakemake, update_config_from_wildcards, create
 
 logger = create_logger(__name__)
 
-def attach_grid_region_to_buses(network, path_shape, distance_crs):
+def attach_emm_region_to_buses(network, path_shape, distance_crs):
     """
-    Attach state to buses
+    Attach emm region to buses
     """
     # Read the shapefile using geopandas
     shape = gpd.read_file(path_shape, crs=distance_crs)
@@ -77,7 +77,7 @@ if __name__ == "__main__":
     regional_fees = pd.read_csv(regional_fees_path)
     network = pypsa.Network(nc_path)
 
-    attach_grid_region_to_buses(network, shape_path, distance_crs)
+    attach_emm_region_to_buses(network, shape_path, distance_crs)
     region_set = network.buses.region.unique()
 
     for region in region_set:
